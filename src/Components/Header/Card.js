b6973b10a5ac2e1c8a02ffe741df497bf10ed435
rahmatalisk <sk.rahmatali6792@gmail.com>
2022-09-21 19:44:13 +0600

    commit

--- src/Components/Header/Card.js
+++ src/Components/Header/Card.js
@@ -11,7 +11,7 @@ const Card = () => {
                 <div className='md:w-1/2'>
                     <h2 className='md:text-4xl text-3xl text-white font-bold'>Find a better card deal in few easy steps.</h2>
                     <p className='text-gray-400 mt-8'>Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.</p>
-                    <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-[#9DEDF0] text-black hover:text-white">Get Started</button>
+                    <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg bg-[#9DEDF0] text-black hover:text-white mt-10">Get Started</button>
                 </div>
                 <div className='md:w-1/2'>
                     <img className='w-auto' src={card} alt="" />
